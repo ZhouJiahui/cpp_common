5fced58f40d4887a7567675c933517d60524449d
zhoujiahui <zjh_coder@qq.com>
2017-07-15 10:09:08 +0800

    feat(blocking_queue): add blocking_queue

---
+++ src/blocking_queue.h
@@ -0,0 +1,56 @@
+#ifndef CPPLY_BLOCKING_QUEUE_H_
+#define CPPLY_BLOCKING_QUEUE_H_
+
+#include <stdint.h>
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <utility>
+
+namespace cpply {
+
+template <typename T>
+class BlockingQueue {
+public:
+  BlockingQueue() = default;
+
+  BlockingQueue(const BlockingQueue&) = delete;
+
+  size_t Size() {
+    std::unique_lock<std::mutex> lock;
+    return que_.size();
+  }
+
+  T Take() {
+    std::unique_lock<std::mutex> lock;
+    // avoid spurious wakeup
+    while (!que_.empty()) {
+      not_empty_.wait(lock); 
+    }
+    T t(std::move(que_.front()));
+    que_.pop_front();
+    return t;
+  }
+
+  void Put(const T& t) {
+    std::unique_lock<std::mutex> lock;
+    que_.push_back(t);
+    not_empty_.notify_all(); 
+  }
+
+  void Put(T&& t) {
+    std::unique_lock<std::mutex> lock;
+    que_.push_back(std::move(t));
+    not_empty_.notify_all(); 
+  }
+
+private:
+  std::deque<T> que_;
+  std::mutex    mutex_;
+  std::condition_variable not_empty_;
+};
+
+}
+
+#endif // CPPLY_BLOCKING_QUEUE_H_
+
